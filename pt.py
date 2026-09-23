ab211dd6ba4d6713fa3f1b1b3090cb9b56de8b3c
gusrlLee <gusrl3204@naver.com>
2026-09-23 20:57:59 +0900

    add asset and fixed path tracing

--- pt.py
+++ pt.py
@@ -7,54 +7,119 @@ mi.set_variant('cuda_ad_rgb')
 class PathTracer(mi.SamplingIntegrator):
     def __init__(self, props):
         super().__init__(props)
-        self.max_depth = 5
+        self.max_depth = 10
+        self.rr_depth = 5
+
+    def mis_weight(self, pdf_a, pdf_b):
+        a2 = pdf_a * pdf_a
+        b2 = pdf_b * pdf_b
+
+        w = a2 / (a2 + b2)
+        return dr.detach(dr.select(dr.isfinite(w), w, 0.0))
 
     @dr.syntax
     def sample(self, scene, sampler, ray, medium=None, active=True):
-        # init 
-        bsdf_ctx = mi.BSDFContext()
-        beta = mi.Spectrum(1.0)
+        # ----------------------------------------------
+        # Initialization 
+        # ----------------------------------------------
+        ctx = mi.BSDFContext()
+        ray = mi.Ray3f(ray)
         L = mi.Spectrum(0.0)
+        beta = mi.Spectrum(1.0)
+
+        eta = mi.Float(1.0)
         depth = mi.UInt32(0)
+
         valid_ray = mi.Bool(False)
-        r = mi.Ray3f(ray)
 
-        while (active):
-            # bounce 
-            si: mi.SurfaceInteraction3f = scene.ray_intersect(r, active)
+        # Information from the previous vertex.
+        prev_si = dr.zeros(mi.Interaction3f)
+        prev_bsdf_pdf = mi.Float(1.0)
+        prev_bsdf_delta = mi.Bool(True)
+
+        # ----------------------------------------------
+        # Path Tracing Main Loop
+        # ----------------------------------------------
+        while active:
+            si = scene.ray_intersect(ray, active=active)
+            emitter = si.emitter(scene, active=active)
+            is_emitter = emitter != None
+
+            # Probability that NEE could have generated 
+            direct_sample = mi.DirectionSample3f(scene, si, prev_si)
+            emitter_pdf = scene.pdf_emitter_direction(
+                prev_si, 
+                direct_sample, 
+                active=active & is_emitter & ~prev_bsdf_delta
+            )
+
+            mis_bsdf = self.mis_weight(prev_bsdf_pdf, emitter_pdf)
+
+            Le = emitter.eval(si, active=active & is_emitter)
+
+            L += beta * Le * mis_bsdf
+            valid_ray |= active & is_emitter
+
             is_hit = si.is_valid()
-            valid_ray = valid_ray | is_hit
-
-            active = active & is_hit
-
-            light = si.emitter(scene, active=active)
-            is_light = light != None 
-
-            Le = light.eval(si, active=(active & is_light))
-            L = L + beta * Le
+            active_next = (active & is_hit & (depth + 1 < self.max_depth))
 
             bsdf = si.bsdf()
-            bs, weight = bsdf.sample(bsdf_ctx, si, sampler.next_1d(), sampler.next_2d(), active)
+            active_emitter = (active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth))
+            ds, emitter_weight = scene.sample_emitter_direction(
+                si, 
+                sampler.next_2d(active_emitter),
+                test_visibility=True,
+                active=active_emitter
+            )
 
-            new_dir = si.to_world(bs.wo)
-            next_ray = si.spawn_ray(new_dir)
+            active_emitter &= ds.pdf > 0
+            wo_emitter = si.to_local(ds.d)
 
-            beta = beta * weight
-            r = next_ray
+            bsdf_value, bsdf_pdf = bsdf.eval_pdf(
+                ctx, 
+                si,
+                wo_emitter,
+                active_emitter
+            )
 
-            depth = depth + 1
-            is_continue = depth < self.max_depth
-            
-            active = active & is_continue
+            mis_emitter = dr.select(ds.delta, 1.0, self.mis_weight(ds.pdf, bsdf_pdf))
 
-        return L, valid_ray, []
+            direct_contribution = (beta * bsdf_value * emitter_weight * mis_emitter)
+            L += dr.select(active_emitter, direct_contribution, 0.0)
+
+            bs, bsdf_weight = bsdf.sample(ctx, si, sampler.next_1d(), sampler.next_2d(), active_next)
+            active_bsdf = active_next & (bs.pdf > 0)
+
+            new_direction = si.to_world(bs.wo)
+            ray = si.spawn_ray(new_direction)
+
+            beta *= bsdf_weight
+            eta *= bs.eta
+
+            non_null = (active_bsdf & ~mi.has_flag(bs.sampled_type, mi.BSDFFlags.Null))
+            valid_ray |= non_null
+
+            prev_si = mi.Interaction3f(si)
+            prev_bsdf_pdf = bs.pdf
+            prev_bsdf_delta = mi.has_flag(bs.sampled_type, mi.BSDFFlags.Delta)
+
+            depth += 1
+
+            # R.R.
+            throughput_max = dr.max(mi.unpolarized_spectrum(beta))
+            rr_probability = dr.minimum(throughput_max * dr.square(eta), 0.95)
+            rr_active = depth >= self.rr_depth
+
+            rr_continue = (sampler.next_1d(rr_active) < rr_probability)
+            beta[rr_active] *= dr.rcp(dr.detach(rr_probability))
+
+            active = (active_bsdf & (throughput_max != 0.0) & (~rr_active | rr_continue))
+
+        return dr.select(valid_ray, L, 0.0), valid_ray, []
 
 
 if __name__ == "__main__":
-    scene = mi.load_file("assets/cornell-box/scene.xml")
+    scene = mi.load_file("assets/glass-of-water/scene.xml")
     integrator = PathTracer(mi.Properties())
-    image = mi.render(scene, integrator=integrator, spp=16)
-
-    plt.axis("off")
-    plt.imshow(image ** (1.0 / 2.2))
-    plt.show()
+    image = mi.render(scene, integrator=integrator, spp=1)
+    mi.util.write_bitmap("path_tracing.png", image)
